319ec4f68316c89cff1e47d849662fb760526867
Cesar Martinez Torres <20808@udla.fundacion.mx>
2024-02-07 11:41:57 -0600

    January 31

--- Codigos_clase/Unidad_2/Jan31.m
+++ Codigos_clase/Unidad_2/Jan31.m
@@ -8,7 +8,7 @@
 % r=rotz(-90,'deg')
 % t=[0,0,0]
 % B=SE3(r,t)
-% figure
+% figure 
 % tranimate(W,A)
 % pause()
 % figure
@@ -21,3 +21,6 @@ r1=trot2(q1)
 t=SE2(25,0,0)
 t1=transl2(25,0)
 aTb=r*t
+q1=deg2rad(35)
+aTb=eval(aTb.T)
+angles=tr2rpy(aTb,'deg')
