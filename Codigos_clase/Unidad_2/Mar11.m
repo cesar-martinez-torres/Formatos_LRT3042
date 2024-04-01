35c54f755d7ef6665b4a232a7c2860abb52efcb8
Cesar Martinez Torres <20808@udla.fundacion.mx>
2024-04-01 12:50:06 -0600

    1 abril

--- Codigos_clase/Unidad_2/Mar11.m
+++ Codigos_clase/Unidad_2/Mar11.m
@@ -13,8 +13,44 @@ izq1=inv(aTb)*noap
 izq1=simplify(izq1)
 der1=bTc*cTd
 der1=simplify(der1)
-% Elementos candidatos 1,4 y 3,4
-% (1,4)
-ca1=izq1(1,4)==der1(1,4)
-ca2=izq1(3,4)==der1(3,4)
-q1sol=solve(ca2,q1)
+ec1=izq1(3,4)==der1(3,4)
+q1_sol1=atan2(-py,-px)
+q1_sol2=atan2(py,px)
+%% Paso 2
+izq2=inv(bTc)*inv(aTb)*noap
+izq2=simplify(izq2)
+der2=cTd
+der2=simplify(der2)
+ec2=izq2(1,4)==der2(1,4)
+ec3=izq2(3,4)==der2(3,4)
+%% Evaluar
+L1=5
+L(1)=Link([0 L1 0 pi/2 0])
+L(2)=Link([0 0 0 -pi/2 0])
+L(3)=Link([0 0 0 0 1])
+L(3).qlim=[0 3]
+ant=SerialLink(L,'name','Ejemplo1')
+Q=[pi pi/4 1.5]
+T=ant.fkine(Q)
+angles=tr2rpy(T,'deg')
+%% 
+T=T.T
+px=T(1,4)
+py=T(2,4)
+py=0
+pz=T(3,4)
+q1_sol1=eval(q1_sol1)
+q1_sol2=eval(q1_sol2)
+q1_sol1g=rad2deg(q1_sol1)
+q1_sol2g=rad2deg(q1_sol2)
+%%
+q1n=q1_sol1
+q1=q1n
+ec2q1s1=eval(ec2)
+ec2q1s1=vpa(ec2q1s1)
+a=-1.06
+b=1.06
+q2_sol1=atan2(a,-b)
+q2_sol2=atan2(-a,b)
+q2_sol1g=rad2deg(q2_sol1)
+q2_sol2g=rad2deg(q2_sol2)
